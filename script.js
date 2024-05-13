d3cd19e5f1600b3498abb2f6ac67ab06753a11b3
Satya Prakash Chakrawarti <satyaprakashchakrawarti12345@gmail.com>
2024-05-13 11:22:20 +0530

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+const box1 = document.querySelector(".rightbox1");
+const box2 = document.querySelector(".rightbox2");
+
+
+
+const overlay1 = document.querySelector(".overlay1");
+const overlay2 = document.querySelector(".overlay2");
+
